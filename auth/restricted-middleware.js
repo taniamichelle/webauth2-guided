5fd11f51c4e8187342bcb9af61497bf087a14339
tania <tanislavens@yahoo.com>
2019-09-17 13:36:04 -0400

    added cookies

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,24 +1,9 @@
-const bcrypt = require('bcryptjs');
-
-const Users = require('../users/users-model.js');
-
 module.exports = (req, res, next) => {
-  const { username, password } = req.headers;
-
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: 'Invalid Credentials' });
-        }
-      })
-      .catch(error => {
-        res.status(500).json({ message: 'Ran into an unexpected error' });
-      });
+  // console.log(req.session.user);
+  // is the user logged in === do we have info about the user in our session
+  if (req.session && req.session.user) {
+    next();
   } else {
-    res.status(400).json({ message: 'No credentials provided' });
+    res.status(401).json({ message: 'You shall not pass!' });
   }
 };
